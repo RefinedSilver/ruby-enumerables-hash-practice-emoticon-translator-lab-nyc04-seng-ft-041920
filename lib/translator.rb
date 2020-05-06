2140b9d714e72583986bffb0ca6df8c51ae3b180
Ian McKenzie <imckenzie777@gmail.com>
2020-05-06 20:35:09 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,6 +6,7 @@ def load_library filepath
   library.each do |key, value|
     empty_hash[key] = {}
     empty_hash[key][:english] = value[0]
+    empty_hash[key][:japanese] = value[1]
 end
 p load_library
 
