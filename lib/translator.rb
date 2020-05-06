bf9a54dccdf7a1902ed3ad04b9772f74ec4f2b5b
Ian McKenzie <imckenzie777@gmail.com>
2020-05-06 20:36:51 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,14 +12,14 @@ def load_library filepath
 end
 p load_library
 
-def get_japanese_emoticon("./lib/emoticons.yml", j_value)
-  require "yaml"
-emoticons.each = YAML.load_file("./lib/emoticons.yml")
-end
-p get_japanese_emoticon
-def get_english_meaning("./lib/emoticons.yml", e_value)
-  require "yaml"
-emoticons.each = YAML.load_file("./lib/emoticons.yml")
-puts " library[meaning][:english] #{library[:meaning][:english]}"
-end
-p get_english_meaning
+# def get_japanese_emoticon("./lib/emoticons.yml", j_value)
+# #   require "yaml"
+# # emoticons.each = YAML.load_file("./lib/emoticons.yml")
+# end
+# p get_japanese_emoticon
+# def get_english_meaning("./lib/emoticons.yml", e_value)
+#   require "yaml"
+# emoticons.each = YAML.load_file("./lib/emoticons.yml")
+# puts " library[meaning][:english] #{library[:meaning][:english]}"
+# end
+# p get_english_meaning
