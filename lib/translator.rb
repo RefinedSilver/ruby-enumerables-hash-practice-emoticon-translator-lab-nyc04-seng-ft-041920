59e73ea6d037efc651e3e565b9da401a561e7123
Ian McKenzie <imckenzie777@gmail.com>
2020-05-06 21:09:13 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,7 +13,6 @@ end
 # pp load_library "./lib/emoticons.yml"
 
 def get_japanese_emoticon(library, emoticon)
-  # code goes here
   emotions = load_library(library)
   emotions.each do |key, value|
     if emotions[key][:english] == emoticon
@@ -24,6 +23,11 @@ def get_japanese_emoticon(library, emoticon)
 end
 
 def get_english_meaning("./lib/emoticons.yml", e_value)
-
-puts " library[meaning][:english] #{library[:meaning][:english]}"
+  emotions = load_library(library)
+  emotions.each do |key, value|
+    if emotions[key][:english] == emoticon
+      return emotions[key][:japanese]
+  end
+  end
+  return "Sorry, that emoticon was not found"
 end
