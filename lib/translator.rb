fa37e47d39f493505fab48ce57f18b2acca60f5f
Ian McKenzie <imckenzie777@gmail.com>
2020-05-06 19:51:30 +0000

    Automatically backed up by Learn

---
+++ lib/translator.rb
@@ -0,0 +1,21 @@
+# require modules here
+require .lib/emoticons.yml
+require "yaml"
+def load_library# array, value
+  hash = {
+    {:english => {}},
+    {:japanese => {}}
+  }
+p load_library
+end
+
+def get_japanese_emoticon("./lib/emoticons.yml", j_value)
+  require "yaml"
+emoticons.each = YAML.load_file("./lib/emoticons.yml")
+end
+p get_japanese_emoticon
+def get_english_meaning("./lib/emoticons.yml", e_value)
+  require "yaml"
+emoticons.each = YAML.load_file("./lib/emoticons.yml")
+end
+p get_english_meaning
